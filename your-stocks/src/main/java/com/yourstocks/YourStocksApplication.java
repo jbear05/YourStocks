62e8ceeed885f59fdea90c688fd465009b28b1f4
Jair Garcia <jairg2005@gmail.com>
2025-09-27 00:38:16 -0400

    Initial Commit: Spring Starter Project with dependencies.

---
+++ your-stocks/src/main/java/com/yourstocks/YourStocksApplication.java
@@ -0,0 +1,13 @@
+package com.yourstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourStocksApplication.class, args);
+	}
+
+}
---
+++ your-stocks/src/test/java/com/yourstocks/YourStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
